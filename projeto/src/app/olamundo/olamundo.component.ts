b87c522579ae6c790eea0b4f3c0c65da294c73b3
Lav1n1a <lavinia.camk01@gmail.com>
2024-07-27 10:10:31 -0400

    first commit

---
+++ projeto/src/app/olamundo/olamundo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-olamundo',
+  standalone: true,
+  imports: [],
+  templateUrl: './olamundo.component.html',
+  styleUrl: './olamundo.component.css'
+})
+export class OlamundoComponent {
+
+}
